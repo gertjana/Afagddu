fb98b326c649c086c4a7788f332a6288f7253f7f
Gertjan Assies <gertjan.assies@gmail.com>
2011-09-09 15:29:11 +0200

    jmonkey simple application setup

---
+++ src/net/addictivesoftware/games/afagddu/Main.java
@@ -0,0 +1,44 @@
+package net.addictivesoftware.games.afagddu;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.RenderManager;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Sphere;
+
+/**
+ * test
+ * @author normenhansen
+ */
+public class Main extends SimpleApplication {
+
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        Sphere p = new Sphere(20,20, 1);
+        Geometry geom = new Geometry("Planet", p);
+        geom.setLocalTranslation(Vector3f.ZERO);
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/SolidColor.j3md");
+        mat.setColor("m_Color", ColorRGBA.Blue);
+        geom.setMaterial(mat);
+
+        rootNode.attachChild(geom);
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        //TODO: add update code
+    }
+
+    @Override
+    public void simpleRender(RenderManager rm) {
+        //TODO: add render code
+    }
+}
